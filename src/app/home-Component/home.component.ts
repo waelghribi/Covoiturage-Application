7a2f4a4c4b2538d2608bfe257245d3833cd27441
fedi <fedibenabdesslem46@gmail.com>
2025-04-28 17:52:16 +0100

    nouvelle template pour le front

--- src/app/home-Component/home.component.ts
+++ src/app/home-Component/home.component.ts
@@ -4,8 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: false,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  features = [
+    {
+      icon: '💰',
+      title: 'Économique',
+      description: 'Partagez les frais de voyage et réduisez vos dépenses de transport.'
+    },
+    {
+      icon: '🌱',
+      title: 'Écologique',
+      description: 'Réduisez votre empreinte carbone en partageant votre véhicule.'
+    },
+    {
+      icon: '👥',
+      title: 'Communautaire',
+      description: 'Rencontrez des personnes partageant les mêmes trajets que vous.'
+    }
+  ];
 }
